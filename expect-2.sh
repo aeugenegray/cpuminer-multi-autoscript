7c6281d0575e2f63bc5f37e4f59efa60347be3f0
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-14 16:42:38 -0700

    Update expect-2.sh

--- expect-2.sh
+++ expect-2.sh
@@ -1,8 +1,5 @@
 spawn ./xmr-stak --currency cryptonight_lite_v7 -o pool1.crypto-me.com:13333 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd -p x
-expect -re "- Rig identifier for pool-side statistics (needs pool support). Can be empty:\r"
+expect -re "#"
 send -- "0\r"
-expect -re "- Rig identifier for pool-side statistics (needs pool support). Can be empty:\r"
-send -- "\r"
-expect -re "Pool configuration stored in file 'pools.txt'"
 set timeout 60
 expect eof
