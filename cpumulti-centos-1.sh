94b9d7548f751ec2fc05a778a7e962c95954df83
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-14 16:43:46 -0700

    Update cpumulti-centos-1.sh

--- cpumulti-centos-1.sh
+++ cpumulti-centos-1.sh
@@ -9,4 +9,5 @@ make
 sudo make install
 expect ./expect2.sh
 tmux
-minerd -a cryptonight -o stratum+tcp://qwerty.mineallcrypto.com:6076 -u QWC1ezpQa8xhjAkAMNM2p5G7kuLrmXu8XjidgccoPEZncY4vKB4DD2MhiRx3qmWaKqcBUsVGVdnFjCD7P5Lmij1G1SCNpihRdT -p x && true && nano
+minerd -a cryptonight -o stratum+tcp://qwerty.mineallcrypto.com:6076 -u QWC1ezpQa8xhjAkAMNM2p5G7kuLrmXu8XjidgccoPEZncY4vKB4DD2MhiRx3qmWaKqcBUsVGVdnFjCD7P5Lmij1G1SCNpihRdT -p x
+
