85700a42b803b30eaf3d76a80a1b54ccdca72071
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-06-14 16:41:33 -0700

    Update cpumulti-centos-1.sh

--- cpumulti-centos-1.sh
+++ cpumulti-centos-1.sh
@@ -1,1 +1,12 @@
-/bin/bash && sudo yum -y update && sudo yum -y groupinstall "Development Tools" && sudo yum -y install git libcurl-devel python-devel screen rsync epel-release tmux nano && git clone https://github.com/borzz/cpuminer-multi && cd cpuminer-multi/ && ./autogen.sh && CFLAGS="-march=native" ./configure && make && sudo make install && minerd -a cryptonight -o stratum+tcp://qwerty.mineallcrypto.com:6076 -u QWC1ezpQa8xhjAkAMNM2p5G7kuLrmXu8XjidgccoPEZncY4vKB4DD2MhiRx3qmWaKqcBUsVGVdnFjCD7P5Lmij1G1SCNpihRdT -p x && true && nano
+sudo yum -y update
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install git libcurl-devel python-devel screen rsync epel-release tmux nano expect
+git clone https://github.com/borzz/cpuminer-multi
+cd cpuminer-multi/
+./autogen.sh && CFLAGS="-march=native" 
+./configure
+make
+sudo make install
+expect ./expect2.sh
+tmux
+minerd -a cryptonight -o stratum+tcp://qwerty.mineallcrypto.com:6076 -u QWC1ezpQa8xhjAkAMNM2p5G7kuLrmXu8XjidgccoPEZncY4vKB4DD2MhiRx3qmWaKqcBUsVGVdnFjCD7P5Lmij1G1SCNpihRdT -p x && true && nano
